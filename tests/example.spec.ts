6a31ac4c85c202ad53bd5577b323810103632de1
Ayobami Joseph <ayo@Ayobamis-MacBook-Pro.local>
2025-12-20 02:06:54 +0900

    feat: update page title and chart heading

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has correct title', async ({ page }) => {
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/D3.js Visualization Project/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has chart heading', async ({ page }) => {
+  await page.goto('/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a heading with the name of the chart.
+  await expect(page.getByRole('heading', { name: 'Sample Pie Chart' })).toBeVisible();
 });
